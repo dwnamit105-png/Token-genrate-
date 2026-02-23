6c87f197406ea9d2f2ea38faf1837ed3ac88c8f2
dwnamit105-png <dwnamit105@gmail.com>
2026-02-24 00:50:36 +0530

    Update Server.js

--- Server.js
+++ Server.js
@@ -8,9 +8,7 @@ const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, '0.0.0.0', () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server running on port ${PORT}`);
 });
